448037815eec489383bc7bfcca8c7cfc25e1c77f
HikaruKobayashi <s-s-s2463@i.softbank.jp>
2020-05-31 13:53:01 +0900

    コメント機能を作成。

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,16 @@
 class CommentsController < ApplicationController
   before_action :logged_in_user
   def create
-    post = School.find(params[:id])
+    post = School.find(params[:school_id])
     @comment = post.comments.build(comment_params)
     @comment.user_id = current_user.id
 
     if @comment.save
       flash[:success] = "コメントしました。"
-      redirect_back(fallback_location: school_path)
+      redirect_back(fallback_location: schools_path)
     else
       flash[:success] = "コメントできませんでした。"
-      redirect_back(fallback_location: school_path)
+      redirect_back(fallback_location: schools_path)
     end
   end
 
